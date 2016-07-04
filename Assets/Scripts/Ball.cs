d782b88d60e01616583773775f5bd73694c1199e
Eric Schreffler <omnicrola@gmail.com>
2016-07-04 14:19:56 -0400

    Added walls and collision physics to ball, walls, and player

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class Ball : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    public int initialVelocity = 400;
 
     // Use this for initialization
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _rigidbody2D.isKinematic = false;
-        _rigidbody2D.AddForce(new Vector2(600, 600));
+        _rigidbody2D.AddForce(new Vector2(initialVelocity, initialVelocity));
     }
 
     // Update is called once per frame
