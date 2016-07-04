d6d50b1f47e7a3c3a272e6bded4e5fa0291440fe
Eric Schreffler <omnicrola@gmail.com>
2016-07-04 12:26:10 -0400

    Initial commit. Added two scenes, one for main menu, and one for the actual game

---
+++ Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameLauncher : MonoBehaviour
+{
+
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene("GameScene");
+
+    }
+}
