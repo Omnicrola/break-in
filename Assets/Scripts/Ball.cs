e0b2ac573dd27d3cb8c08f2ced05263540780f75
Eric Schreffler <omnicrola@gmail.com>
2016-07-04 13:27:07 -0400

    Added prefabs for ball, player, and brick. Player sprite tracks the X position of the mouse.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(new Vector2(600, 600));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float yPosition = 10;
+
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+        rigidBody.MovePosition(new Vector2(x, yPosition));
+    }
+}
